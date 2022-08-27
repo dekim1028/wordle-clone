b170692be5edb8a963a834235fdbdc796880033a
dekim1028 <daeun235194@gmail.com>
2022-08-27 13:46:50 +0900

    🎨 : styled-components structure

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  color: {
+    green: '#6aaa64',
+    yellow: '#c9b458',
+    gray: '#787c7e',
+  },
+};
+
+export type Theme = typeof theme;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as STC from './MainPage.style';
+
+const MainPage = () => {
+  return <STC.Container>MainPage</STC.Container>;
+};
+
+export default MainPage;
