80a1728d994fe2a9c9d613bffe063b88e1d35ec7
dekim1028 <daeun235194@gmail.com>
2022-08-27 15:23:48 +0900

    🍱 : GlobalStyle, theme

--- src/assets/styles/GlobalStyle.ts
+++ src/assets/styles/GlobalStyle.ts
@@ -3,6 +3,15 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  * {
+    margin: 0;
+    padding: 0;  
+  }
+
+  body {
+    box-sizing: border-box;
+  }
 `;
 
 export default GlobalStyle;
--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,8 +1,22 @@
 export const theme = {
-  color: {
+  tile: {
     green: '#6aaa64',
     yellow: '#c9b458',
     gray: '#787c7e',
+  },
+  color: {
+    gray01: '#000000',
+    gray02: '#787c7e',
+    gray03: '#878a8c',
+    gray04: '#d3d6da',
+    gray05: '#edeff1',
+    gray06: '#f6f7f8',
+    gray07: '#ffffff',
+    gray08: '#121212',
+    gray09: '#dfdfdf',
+    gray10: '#787c7e',
+    gray11: '#363636',
+    white: '#ffffff',
   },
 };
 
